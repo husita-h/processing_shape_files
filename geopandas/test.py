5e1b187d6cd4304141a71c65fac1cd62fa34c83a
husita-h <taisei.fukushi1129@gmail.com>
2022-09-01 23:43:20 +0900

    液状化予測図をshapeファイルを変換

--- geopandas/test.py
+++ geopandas/test.py
@@ -5,9 +5,16 @@
 
 import geopandas as gpd
 
-path_shp = "../data/pl_distribution_map/liqpt.shp"
-gdf = gpd.read_file(path_shp)
-gdf.to_file("./data/output_utf-8.geojson", driver='GeoJSON', encoding="utf-8")
-gdf.to_file("./data/output_shift-jis.geojson", driver='GeoJSON', encoding="shift-jis")
-gdf.to_csv("./data/output_utf-8.csv")
-gdf.to_csv("./data/output_shift-jis.csv")
+path_shp_pl_distribution_map = "../data/pl_distribution_map/liqpt.shp"
+gdf = gpd.read_file(path_shp_pl_distribution_map)
+gdf.to_file("./data_pl_distribution_map/output_utf-8.geojson", driver='GeoJSON', encoding="utf-8")
+gdf.to_file("./data_pl_distribution_map/output_shift-jis.geojson", driver='GeoJSON', encoding="shift-jis")
+gdf.to_csv("./data_pl_distribution_map/output_utf-8.csv")
+gdf.to_csv("./data_pl_distribution_map/output_shift-jis.csv")
+
+path_shp_liquefaction_forecast_map = "../data/liquefaction_forecast_map/eqliq.shp"
+gdf = gpd.read_file(path_shp_liquefaction_forecast_map)
+gdf.to_file("./data_liquefaction_forecast_map/output_utf-8.geojson", driver='GeoJSON', encoding="utf-8")
+gdf.to_file("./data_liquefaction_forecast_map/output_shift-jis.geojson", driver='GeoJSON', encoding="shift-jis")
+gdf.to_csv("./data_liquefaction_forecast_map/output_utf-8.csv")
+gdf.to_csv("./data_liquefaction_forecast_map/output_shift-jis.csv")
